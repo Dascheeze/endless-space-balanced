d80115e93cba24d500bdb9021a9ce6f2bc836782
Richard Tracy <thebigcheeze@gmail.com>
2012-08-11 13:44:36 -0700

    Fixed nonrecursive copy.

--- deploy.ps1
+++ deploy.ps1
@@ -4,7 +4,7 @@ function deploy-endless-space-balanced(){
 	$userProfile = $env:userprofile
 	$deployDir = $userProfile + "\Documents\Endless Space\Modding\"
 	
-	cp .\BalanceMod $deploy
+	cp -recurse .\BalanceMod $deploy
 }
 
 deploy-endless-space-balanced()
